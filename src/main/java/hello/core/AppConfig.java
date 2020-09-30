09cc7f67433bb07e356c23eb381fc62483657c8e
SongMinJun <dkaskgkdua@gmail.com>
2020-09-30 15:24:29 +0900

    스프링 전환
    - Configuration 선언
    - Bean 선언
    - Bean 객체 가져오기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /*
  *  Denpendency Injection 해준다!
+ *  기존에는 개발자가 직접 DI를 했지만 이제는 스프링 컨테이너를 통해서 사용한다.
+ *  스프링 컨테이너는 @Configuration이 붙은 AppConfig를 설정 정보로 사용한다.
+ *  여기서 @Bean 이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
+ *  이를 스프링 빈이라고 함.
+ *  스프링 빈은 @Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용함.(memberService, orderService)
  * */
-
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
