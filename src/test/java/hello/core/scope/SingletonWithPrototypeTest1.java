b0051d26b7b1ec24b0c597f05fcda7270628cf65
SongMinJun <dkaskgkdua@gmail.com>
2020-10-04 00:26:35 +0900

    ObjectProvider, Provider 사용
    프로토타입 빈을 언제 사용해야하는가...?

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,14 +4,31 @@ import ch.qos.logback.core.net.server.Client;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 프로토타입 빈을 언제 사용할까?
+ * 매번 사용할 때 마다 의존관계 주입이 완료된 새로운 객체가 필요하면 사용하면 된다. 그런데
+ * 실무에서 웹 어플리케이션을 개발해보면, 싱글톤 빈으로 대부분의 문제를 해결할 수 있기 때문에
+ * 프로토타입 빈을 직접적으로 사용하는 일이 매우 드물다.
+ * ex) 서로가 의존관계일 경우...
+ *
+ * ObjectProvider, JSR330 Provider 등은 프로토타입 뿐만 아니라 DL이 필요한 경우 언제든
+ * 사용 가능하다.
+ *
+ * 스프링이 제공하는 메서드에 @Lookup 어노테이션을 사용하는 방법도 있긴 있지만 이전 방법들로
+ * 충분하다.
+ *
+ * */
 
 public class SingletonWithPrototypeTest1 {
     @Test
@@ -44,15 +61,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+        // 지정한 빈을 컨테이너에서 대신 찾아주는 DL 서비스를 제공하는 것이 바로 ObjectProvider
+        // 과거엔 ObjectFactory를 사용했는데 여기에 확장된 것이 ObjectProvider
+        // getObject() 를 호출하면 내부에서는 스프링컨테이너를 통해 해당 빈을 찾아서 반환함.
+        // ApplicationContext에서 전체를 가져오는 것이 아닌 DL 기능만 제공하는 ObjectProvider 사용!
+        @Autowired
+        //private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
